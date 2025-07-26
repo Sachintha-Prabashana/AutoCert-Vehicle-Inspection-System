4181779242f2012c8e817865bdb2cd1e64b8faf7
Sachintha Prabashana <sachinthaprabhashana2003@gmail.com>
2025-07-26 22:21:07 +0530

    Initialize Spring Boot project with Maven wrapper, configuration files, and basic structure

---
+++ src/main/java/lk/ijse/autocert/AutocertApplication.java
@@ -0,0 +1,13 @@
+package lk.ijse.autocert;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutocertApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutocertApplication.class, args);
+	}
+
+}
---
+++ src/test/java/lk/ijse/autocert/AutocertApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.ijse.autocert;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutocertApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
